e56310acbd44037912551085eec3e40d0bc87fd3
Dmitry <dmitryavgn@mail.ru>
2023-04-18 18:08:02 +0500

    сделал вертикальный и горизонтальные слайдеры

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,7 @@
-import { modals } from './modules';
+import { modals, sliders } from './modules';
 
 window.addEventListener('DOMContentLoaded', () => {
     modals()
+    sliders({slides: '.feedback-slider-item', dir: 'horizontal', prev: '.main-prev-btn', next: '.main-next-btn'})
+    sliders({slides: '.main-slider-item', dir: 'vertical', prev: '', next: ''})
 })
