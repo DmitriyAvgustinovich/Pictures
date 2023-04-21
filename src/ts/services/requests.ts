205ea97d41c46e9273378e5d87ea1824a4d74bdb
Dmitry <dmitryavgn@mail.ru>
2023-04-21 13:44:27 +0500

    поработал над формой калькулятором и её отправкой на сервер

--- src/ts/services/requests.ts
+++ src/ts/services/requests.ts
@@ -1,4 +1,4 @@
-export const postData = async (url: string, data) => {
+export const postData = async (url: string, data: any) => {
     const res = await fetch(url, {
         method: 'POST',
         headers: {
